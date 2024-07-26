52a56a83c0deabe0a8fc589b584d8d792979b1dd
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2024-07-25 21:12:04 -0300

    Conceitos Básicos

---
+++ Introdução/PrimeiraFormaNormal/PrimeiraFormaNormal.sql
@@ -0,0 +1,6 @@
+/*
+    PRIMEIRA FORMA NORMAL 
+        1. Todo campo vetorizado se tornará outra tabela.
+        2. Todo campo multivalorado se tornará outra tabela quando o campo for divisível.
+        3. Toda tabela necessita de pelo menos um campo que identifique todo o registro como sendo único. (Primary Key)
+*/
