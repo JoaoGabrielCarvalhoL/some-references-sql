2fd8e33504230f5d47d95dc5dd82d641b57c4206
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2024-07-31 16:44:57 -0300

    Examples functions

---
+++ Introdução/Functions/fun.sql
@@ -0,0 +1,17 @@
+/*
+    Max: Retorna o valor máximo de uma coluna.
+    SELECT MAX(<COLUNA>) FROM <NOME TABELA>
+
+    Min: Retorna o valor mínimo de uma coluna.
+    SELECT MIN(<COLUNA>) FROM <NOME TABELA>
+
+    AVG: Retorna a média aritmética de uma coluna.
+    SELECT AVG(<COLUNA>) FROM <NOME TABELA>
+
+    Truncate: Arredonda valores númericos. 
+    Params: (Número a ser arredondado, número de casas decimais após a vírgula)
+
+    Sum: Retorna a soma dos valores de uma coluna.
+    SELECT SUM(<COLUNA>) FROM <NOME TABELA>
+
+*/
