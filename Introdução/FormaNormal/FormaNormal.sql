5aca3a0755e423e132cc69b7966ccc0b2ec90d76
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2024-08-01 19:40:41 -0300

    Another normal forms

--- Introdução/FormaNormal/FormaNormal.sql
+++ Introdução/FormaNormal/FormaNormal.sql
@@ -3,4 +3,12 @@
         1. Todo campo vetorizado se tornará outra tabela.
         2. Todo campo multivalorado se tornará outra tabela quando o campo for divisível.
         3. Toda tabela necessita de pelo menos um campo que identifique todo o registro como sendo único. (Primary Key)
+
+    SEGUNDA FORMA NORMAL
+        1. Dependência Direta.
+        2. Campos não chave precisam depender da totalidade da chave.
+
+    TERCEIRA FORMA NORMAL
+        1. Dependência Transitiva.
+        2. Campos não chave que dependem de outros campos não chave, se tornarão outra tabela.
 */
